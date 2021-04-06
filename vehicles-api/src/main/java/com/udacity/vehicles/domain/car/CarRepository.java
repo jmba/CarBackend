87091e53e3527ba2d6d0eaf068810ea287e13d1d
jmba <jm.bayerl@googlemail.com>
2021-04-06 22:26:31 +0200

    Extended car repository.

--- vehicles-api/src/main/java/com/udacity/vehicles/domain/car/CarRepository.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/domain/car/CarRepository.java
@@ -1,9 +1,12 @@
 package com.udacity.vehicles.domain.car;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
-
+    @Query("SELECT c FROM Car c WHERE c.id=:id")
+    //@Query("select * from car d where d.id=:id")
+    Car findCarById(Long id);
 }
